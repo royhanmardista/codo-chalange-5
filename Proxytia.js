ba634dc5bf865a5004f036f551a69355714bff19
Royhan Mardista <royhanm23@gmail.com>
2019-08-27 10:51:23 +0700

    -- fixing some syntaxs

--- Proxytia.js
+++ Proxytia.js
@@ -5,22 +5,22 @@ var peran = "dasda";
 /* peran akan diisi sesuai dengan pilihan yang ada*/
 if (peran == "") {
     console.log("Halo", nama, "Pilih peranmu untuk memulai game!");
-    console.log("Pilih diantara tiga peran ini Ksatria, Tabib, Penyihir")
+    console.log("Pilih diantara tiga peran ini Ksatria, Tabib, Penyihir");
 }
 else {
     if (peran == "Ksatria") {
-        console.log ("Selamat datang di Dunia Proxytia", nama)
-        console.log("Halo Ksatria", nama, "kamu dapat menyerang dengan senjatamu!")
+        console.log ("Selamat datang di Dunia Proxytia", nama);
+        console.log("Halo Ksatria", nama, "kamu dapat menyerang dengan senjatamu!");
     }
     else if (peran == "Tabib") {
-        console.log ("Selamat datang di Dunia Proxytia", nama)
-        console.log("Halo Tabib", nama, "kamu akan membantu temanmu yang terluka.")
+        console.log ("Selamat datang di Dunia Proxytia", nama);
+        console.log("Halo Tabib", nama, "kamu akan membantu temanmu yang terluka.");
     }
     else if (peran == "Penyihir") {
-        console.log ("Selamat datang di Dunia Proxytia", nama)
-        console.log("Halo Penyihir", nama, "ciptakan keajaiban yang membantu kemenanganmu!")
+        console.log ("Selamat datang di Dunia Proxytia", nama);
+        console.log("Halo Penyihir", nama, "ciptakan keajaiban yang membantu kemenanganmu!");
     }
     else {
-        console.log ("Silakan masukkan peran yang telah disediakan")
+        console.log ("Silakan masukkan peran yang telah disediakan");
     }
 }
